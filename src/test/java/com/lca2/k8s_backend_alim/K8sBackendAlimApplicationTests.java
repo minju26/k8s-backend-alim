43b541bebb6c0e1305a23bcf88622725dc46b67a
minju26 <mjalswn26@gmail.com>
2025-06-22 17:36:01 +0900

    Initial commit

---
+++ src/test/java/com/lca2/k8s_backend_alim/K8sBackendAlimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lca2.k8s_backend_alim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sBackendAlimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
